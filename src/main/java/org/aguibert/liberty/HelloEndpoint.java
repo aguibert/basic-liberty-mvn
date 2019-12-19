0aee91b122343d9d320a447b96f2a15b89af17f2
Andrew Guibert <andy.guibert@gmail.com>
2019-12-19 13:37:11 -0600

    Initial progress on secured endpoint

--- src/main/java/org/aguibert/liberty/HelloEndpoint.java
+++ src/main/java/org/aguibert/liberty/HelloEndpoint.java
@@ -1,15 +1,20 @@
 package org.aguibert.liberty;
 
+import javax.annotation.security.DeclareRoles;
+import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/")
 @ApplicationScoped
+@DeclareRoles("Administrator")
+@RolesAllowed("Administrator")
 public class HelloEndpoint {
 
     @GET
     public String sayHello() {
+        System.out.println("Hello world");
         return "Hello World";
     }
 }
